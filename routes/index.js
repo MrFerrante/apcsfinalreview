2185c6360e901c3b543364aa58737e3cbcca97d5
ask616 <areeb.khan616@gmail.com>
2015-12-08 21:36:59 -0800

    final?

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/new', function(req, res) {
   res.render('new', { title: 'Express' });
 });
 
-router.get('/new-submit', function(req, res) {
-  res.render('new-submit', { title: 'Express' });
-});
-
 router.get('/submit', function(req, res) {
-  res.render('submit', { title: 'Express' });
+  res.render('new-submit', { title: 'Express' });
 });
 
 router.get('/administr', function(req, res) {
