efc15b5a28299c2c15449801a82f4ea29dde7729
ask616 <areeb.khan616@gmail.com>
2015-12-10 04:31:44 -0800

    final

--- routes/index.js
+++ routes/index.js
@@ -2,23 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('new', { title: 'Express' });
+  res.render('new', { title: 'APCS' });
 });
 
 router.get('/test', function(req, res) {
-  res.render('test', { title: 'Express' });
+  res.render('test', { title: 'APCS' });
 });
 
 router.get('/submit', function(req, res) {
-  res.render('new-submit', { title: 'Express' });
+  res.render('new-submit', { title: 'APCS' });
 });
 
 router.get('/test-submit', function(req, res) {
-  res.render('test-submit', { title: 'Express' });
+  res.render('test-submit', { title: 'APCS' });
 });
 
 router.get('/administr', function(req, res) {
-  res.render('new-admin', { password: 'rainySparkyDays' });
+  res.render('new-admin', { title: 'APCS' });
 });
 
 module.exports = router;
