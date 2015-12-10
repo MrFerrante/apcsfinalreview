9670ceff0641fa2cc0c5beb4d3e570039162acf6
ask616 <areeb.khan616@gmail.com>
2015-12-09 17:16:37 -0800

    test editing

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,16 @@ router.get('/', function(req, res) {
   res.render('new', { title: 'Express' });
 });
 
+router.get('/test', function(req, res) {
+  res.render('new', { title: 'Express' });
+});
+
 router.get('/submit', function(req, res) {
   res.render('new-submit', { title: 'Express' });
+});
+
+router.get('/test-submit', function(req, res) {
+  res.render('new', { title: 'Express' });
 });
 
 router.get('/administr', function(req, res) {
